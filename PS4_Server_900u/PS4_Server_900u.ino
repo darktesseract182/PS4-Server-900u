1cf421503978806e349669557da294fc918c2d91
darktesseract182 <caballoruciomoro69@gmail.com>
2024-02-11 16:28:14 -0400

    Add mjs fiel extension

--- PS4_Server_900u/PS4_Server_900u.ino
+++ PS4_Server_900u/PS4_Server_900u.ino
@@ -226,6 +226,7 @@ String getContentType(String filename){
   else if(filename.endsWith(".html")) return "text/html";
   else if(filename.endsWith(".css")) return "text/css";
   else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".mjs")) return "application/javascript";
   else if(filename.endsWith(".png")) return "image/png";
   else if(filename.endsWith(".gif")) return "image/gif";
   else if(filename.endsWith(".jpg")) return "image/jpeg";
